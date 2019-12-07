4f39db150bc6f32a1d11458bf6f831671dcc4a43
cabral <cabral@moncholo>
2019-12-07 03:54:12 -0300

    basic tilemap and jump system

--- tilemap.c
+++ tilemap.c
@@ -3,21 +3,19 @@
 
 
 
-void load_tileset(unsigned int tileset, int size)
+void load_tileset(unsigned int tileset, int size, int bank)
 {
-	CopyToVRAM((unsigned int) tileset, BASE12, size); //character table
+	CopyToVRAM((unsigned int) tileset, BASE12+bank, size); //character table
 }
 
 
-void load_colormap(unsigned int tileset, int size)
+void load_colormap(unsigned int tileset, int size, int bank)
 {
-	//FillVRAM(BASE11, 100, LIGHT_GREEN); //color table
-	CopyToVRAM((unsigned int) tileset, BASE11, size);
+	CopyToVRAM((unsigned int) tileset, BASE11+bank, size);
 }
 
 
 void load_tilemap(unsigned int tileset, int size)
 {
-	//FillVRAM(BASE10, 768, 0x00); //name table
 	CopyToVRAM((unsigned int) tileset, BASE10, size);
 }
--- tilemap.h
+++ tilemap.h
@@ -2,8 +2,8 @@
 #define  __TILEMAP_H__
 
 
-void load_tileset(unsigned int tileset, int size);
-void load_colormap(unsigned int tileset, int size);
+void load_tileset(unsigned int tileset, int size, int bank);
+void load_colormap(unsigned int tileset, int size, int bank);
 void load_tilemap(unsigned int tileset, int size);
 
 
